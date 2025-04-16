dbffb13ed3687b16ce10fb50f35b7128377b8006
mays66alshaikhli <150714954+mays66alshaikhli@users.noreply.github.com>
2025-04-16 10:26:29 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,5 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+export default [
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]
 
 export default eslintConfig;
